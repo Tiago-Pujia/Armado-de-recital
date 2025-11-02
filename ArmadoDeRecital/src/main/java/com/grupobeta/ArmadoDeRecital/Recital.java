02c41dd17a544e8f32a944ff38695d1476d79955
Lautaro-Barreto <barretolautaroagustin36@gmail.com>
2025-11-02 17:07:03 -0300

    Añadido esqueleto de Recital.java

---
+++ ArmadoDeRecital/src/main/java/com/grupobeta/ArmadoDeRecital/Recital.java
@@ -0,0 +1,42 @@
+package com.grupobeta.ArmadoDeRecital;
+
+import java.util.ArrayList;
+
+public class Recital {
+	
+	ArrayList<Contratacion> contrataciones = null;
+	
+	public Recital() {
+		this.contrataciones = new ArrayList<Contratacion>();
+	}
+	
+	///punto 1
+	public void obtenerRolesFaltantesParaCancion() {
+		
+	}
+	
+	///punto 2
+	public void obtenerRolesFaltantesAll() {
+		
+	}
+	
+	///punto 3
+	public void contratarArtistasParaCancion() {
+		
+	}
+	
+	///punto 4
+	public void contratarArtistasAll() {
+		
+	}
+	
+	///punto 6
+	public void listarArtistasContratados() {
+		
+	}
+	
+	///punto 7
+	public void listarCanciones() {
+		
+	}
+}
